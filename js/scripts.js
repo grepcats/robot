cfa78848c822c8f34084ded3634e73ce94db373f
Kayla Ondracek <kayla.renee@gmail.com>
2018-01-19 12:45:20 -0800

    add styling

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ function zeroOne(input, name) {
   if (name === "") {
     var name = "friend";
   }
-  
+
   if (input % 3 === 0 && input !== 0) {
     var input = "I'm sorry " + name + ", I'm afraid I can't do that"
     return input;
@@ -58,7 +58,9 @@ $(document).ready(function() {
     var nameInput = $("#name").val();
 
     if (numberCheck(userInput) !== false) {
+      $("#results").show();
       $(".wrong").remove();
+      $(".robot").html("<img src='img/robot.svg'/>");
       $("#results").append("<div class='output'><p>You entered: " + userInput + ".")
       var output = transform(inputArray(userInput), nameInput);
       output.forEach(function(number) {
@@ -66,7 +68,9 @@ $(document).ready(function() {
       });
     } else {
       $(".wrong").remove();
+      $("#results").show();
       $("#results").append("<div class='wrong'><p>That's not a number! Try again</p>")
+      $(".robot").html("<img src='img/angry-robot.svg' id='angry'/>");
     }
 
   });
