875226a7988772b8ce1da58c34c95b19a7ce9ae8
Kayla Ondracek <kayla.renee@gmail.com>
2018-01-19 16:08:31 -0800

    adjust styling, organize js

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,38 @@
-//takes a string/number and replaces it with other content
-function zeroOne(input, name) {
-  //var input = numberCheck(input);
-  if (name === "") {
-    var name = "friend";
-  }
+//business logic
+var subNum = 0;
 
+//takes a number and replaces it with other content
+function replaceZeroOneThree(input, name) {
+  if (name === "") {
+    name = "friend";
+  }
   if (input % 3 === 0 && input !== 0) {
-    var input = "I'm sorry " + name + ", I'm afraid I can't do that"
+    input = "I'm sorry " + name + ", I'm afraid I can't do that";
     return input;
     } else if ((/[1]/).test(input)) {
-      var input = "Boop";
+      input = "Boop";
       return input;
     } else if ((/[0]/).test(input)) {
-      var input = "Beep";
+      input = "Beep";
       return input;
     } else {
       return input;
     }
 }
 
-//returns array of range of numbers from 0 to input.
-function inputArray(input) {
-  var countTo = []
+//takes a number and returns array of range of numbers from 0 to input.
+function numberArray(input) {
+  var countTo = [];
   for (var i = 0; i <= input; i++) {
     countTo.push(i);
   }
   return countTo;
 }
 
-//take array of numbers from zeroOne and output changes
+//takes array of content, transforms with replaceZeroOneThree and outputs new array
 function transform(input, name) {
   var transformedArray = input.map(function(number) {
-    return zeroOne(number, name);
+    return replaceZeroOneThree(number, name);
   });
   return transformedArray;
 }
@@ -41,61 +42,59 @@ function reverse(input) {
   return input.reverse();
 }
 
+//user interface logic
 $(document).ready(function() {
-  var subNum = 0;
 
   $("#number-input").submit(function(event) {
     event.preventDefault();
+
     $(".output").remove();
     var userInput = parseInt($("#number").val());
     var nameInput = $("#name").val();
     var revNum = 0;
 
-
+    //evaluates userInput to ensure it is a number before executing rest of code
     if (!isNaN(userInput)) {
-      console.log(userInput);
-      subNum = 0;
+      subNum = 0; //reset submit number so robot is happy again
       $("#results").show();
       $(".wrong").remove();
-      $(".robot").html("<img src='img/robot.svg'/>");
-      $("#results").append("<div class='output'><h3>You entered: " + userInput)
-      var output = transform(inputArray(userInput), nameInput);
+      $(".robot").html("<img src='img/robot.svg' alt='happy robot'/>");
+      $("#results").append("<div class='output'><h3>You entered: " + userInput);
+      var output = transform(numberArray(userInput), nameInput);
       output.forEach(function(number) {
-        $(".output").append(number + "<br/>")
+        $(".output").append(number + "<br/>");
       });
       $("#reverse").show();
 
-
+      //reverse output on click
       $("#reverse-click").click(function() {
-        revNum++;
         var reversed = reverse(output);
         $(".output").remove();
-        if (revNum === 0) {
-          $("#results").append("<div class='output'><h3>Reversed " + userInput + "!")
-        } else if (revNum > 0 && revNum < 5) {
-          $("#results").append("<div class='output'><h3>Reversed " + userInput + " again, good job!")
-        } else {
-          $("#results").append("<div class='output'><h3>Ok calm down, " + nameInput + ".")
-        }
 
+        //header displays different text depending on number of times reversed
+        if (revNum === 0) {
+          $("#results").append("<div class='output'><h3>Reversed " + userInput + "!");
+        } else if (revNum > 0 && revNum < 5) {
+          $("#results").append("<div class='output'><h3>Reversed " + userInput + " again, good job!");
+        } else {
+          $("#results").append("<div class='output'><h3>Ok calm down, " + nameInput + ".");
+        }
+        revNum++;
         reversed.forEach(function(number) {
-          $(".output").append(number + "<br/>")
+          $(".output").append(number + "<br/>");
         });
+
       });
     } else {
+      //handle user submitting non-numeric content
       subNum++;
       $(".wrong").remove();
       $("#results").show();
-      $("#results").append("<div class='wrong'><h3>That's not a number! Try again</h3>")
+      $("#results").append("<div class='wrong'><h3>That's not a number! Try again</h3>");
       $(".robot").html("<img src='img/angry-robot.svg' class='robot-replace'/>");
       if (subNum >= 5) {
         $(".robot").html("<img src='img/v-angry-robot.svg' class='robot-replace'/>");
       }
     }
-
   });
-
-
-
-
 });
